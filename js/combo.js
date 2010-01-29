dd66a84718f46c29af735b69f51748d39525152b
richard Rossel <henhiskan@gmail.com>
2010-01-29 17:27:10 -0300

    Codigo con numero y texto (y un bonus)
    Se codigo volvio a como era antes, es decir mostrando
    la descripcion. Para que se vea el numero y descripcion
    del evento es solo una modificacion en la data de la BD
    haciendo:
    update core_codigos set descripcion = id ||' - ' descripcion ;
    
    Ademas se agrego la opcion de que se pueda tipear para facilitar la
    busqueda, matcheando en cualquier parte del texto ( no solo al comienzo
    del texto como estaba antes)
    
    Closes GH-23

---
+++ js/combo.js
@@ -0,0 +1,53 @@
+/*
+ * Ext JS Library 2.2.1
+ * Copyright(c) 2006-2009, Ext JS, LLC.
+ * licensing@extjs.com
+ * 
+ * http://extjs.com/license
+ */
+
+
+Ext.override(Ext.form.ComboBox, {
+    doQuery : function(q, forceAll){
+        if(q === undefined || q === null){
+            q = '';
+        }
+        var qe = {
+            query: q,
+            forceAll: forceAll,
+            combo: this,
+            cancel:false
+        };
+        if(this.fireEvent('beforequery', qe)===false || qe.cancel){
+            return false;
+        }
+        q = qe.query;
+        forceAll = qe.forceAll;
+        if(forceAll === true || (q.length >= this.minChars)){
+            if(this.lastQuery !== q){
+                this.lastQuery = q;
+                if(this.mode == 'local'){
+                    this.selectedIndex = -1;
+                    if(forceAll){
+                        this.store.clearFilter();
+                    }else{
+                this.anyMatch = this.anyMatch === undefined? false : this.anyMatch;
+                this.caseSensitive = this.caseSensitive === undefined? false : this.caseSensitive;
+                        this.store.filter(this.displayField, q, this.anyMatch, this.caseSensitive);
+                    }
+                    this.onLoad();
+                }else{
+                    this.store.baseParams[this.queryParam] = q;
+                    this.store.load({
+                        params: this.getParams(q)
+                    });
+                    this.expand();
+                }
+            }else{
+                this.selectedIndex = -1;
+                this.onLoad();
+            }
+        }
+    }
+}); 
+
